b570cc9144511dc13c8a9f25692b3707173e53b8
David Jack <davars@gmail.com>
2016-03-17 10:52:43 -0400

    Merge, don't clobber (ie, only set if the current values are nil)

--- lib/db_charmer/rails42/active_record/relation/merger.rb
+++ lib/db_charmer/rails42/active_record/relation/merger.rb
@@ -10,9 +10,9 @@ module DbCharmer
 
         def merge_with_db_charmer
           merge_without_db_charmer.tap do |rel|
-            rel.db_charmer_connection = @other.db_charmer_connection
-            rel.db_charmer_enable_slaves = @other.db_charmer_enable_slaves
-            rel.db_charmer_connection_is_forced = @other.db_charmer_connection_is_forced
+            rel.db_charmer_connection = @other.db_charmer_connection if rel.db_charmer_connection.nil?
+            rel.db_charmer_enable_slaves = @other.db_charmer_enable_slaves if rel.db_charmer_enable_slaves.nil?
+            rel.db_charmer_connection_is_forced = @other.db_charmer_connection_is_forced if rel.db_charmer_connection_is_forced.nil?
           end
         end
       end
